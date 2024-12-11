12b3c7901063dca42c1025dddabc66cef5814a2b
MathSena <MathSena07@hotmail.com>
2024-12-10 23:24:14 -0300

    Initial commit

---
+++ src/main/java/com/mathsena/cadastroplay/CadastroplayApplication.java
@@ -0,0 +1,13 @@
+package com.mathsena.cadastroplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroplayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mathsena/cadastroplay/CadastroplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathsena.cadastroplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
